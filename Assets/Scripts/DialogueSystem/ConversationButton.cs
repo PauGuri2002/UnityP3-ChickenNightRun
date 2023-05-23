4eb3fed79a2cbd7541cbc9b525915f1422b31b23
Pau Guri <pauguvi@gmail.com>
2023-05-23 12:27:23 +0200

    Dialogue WIP

--- Assets/Scripts/DialogueSystem/ConversationButton.cs
+++ Assets/Scripts/DialogueSystem/ConversationButton.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using static DialogueEventManager;
 
 [System.Serializable]
 public class ConversationButton
 {
     public string text;
     public int targetNodeIndex;
+    public ChickenEvent onClickEvent;
 }
--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -23,5 +24,49 @@ public class DialogueManager : MonoBehaviour
         dialogueObject.SetActive(true);
         dialogueText.text = conversation.nodes[0].text;
         speakerText.text = conversation.nodes[0].speakerName;
+    }
+
+    private IEnumerator WriteText(string dialogueText)
+    {
+        char[] textArr = dialogueText.ToCharArray();
+        text.text = "";
+
+        while (text.text.Length < dialogueText.Length)
+        {
+            text.text += textArr[text.text.Length];
+            yield return new WaitForSeconds(typingDelay);
+        }
+        dialogueIndex++;
+        writingCoroutine = null;
+    }
+
+    private void Interact(PlayerCore player)
+    {
+        if (currentDialogue == null) { return; }
+
+        if (writingCoroutine != null) // if the current section of dialoge has not finished getting written
+        {
+            StopCoroutine(writingCoroutine);
+            writingCoroutine = null;
+            text.text = currentDialogue.parts[dialogueIndex];
+            dialogueIndex++;
+        }
+        else if (dialogueIndex < currentDialogue.parts.Length) // otherwise, if there are more sections of dialogue to be written
+        {
+            writingCoroutine = StartCoroutine(WriteText(currentDialogue.parts[dialogueIndex]));
+        }
+        else // otherwise, if the dialogue is done
+        {
+            if (bgAnimationCoroutine != null)
+            {
+                StopCoroutine(bgAnimationCoroutine);
+                bgAnimationCoroutine = null;
+            }
+
+            finishCallback?.Invoke(null);
+            UIManager.OnUISelect -= Interact;
+            PlayerManager.Instance.ChangeInputMapAll("Player");
+            Destroy(gameObject);
+        }
     }
 }
